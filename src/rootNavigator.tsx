7d1a5625b9f5ff2ad986cc06681433d2a3730a9b
Alatise Oluwaseun <holynation667@gmail.com>
2020-10-26 01:04:18 +0100

    Update rootNavigator.tsx
    remove duplicate @react-navigation/native

--- src/rootNavigator.tsx
+++ src/rootNavigator.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { DefaultTheme, DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { useTheme } from 'react-native-paper';
 
 import { StackNavigator } from './stack';
